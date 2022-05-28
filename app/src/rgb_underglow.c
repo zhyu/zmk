761e10409887e2c3db2bf9240665aa80aa4f6f90
Chris Andreae <chris@andreae.gen.nz>
2022-05-28 20:15:51 +0900

    Smoother refresh rate for RGB underglow

--- app/src/rgb_underglow.c
+++ app/src/rgb_underglow.c
@@ -265,7 +265,7 @@ static int zmk_rgb_underglow_init(const struct device *_arg) {
     settings_load_subtree("rgb/underglow");
 #endif
 
-    k_timer_start(&underglow_tick, K_NO_WAIT, K_MSEC(50));
+    k_timer_start(&underglow_tick, K_NO_WAIT, K_MSEC(25));
 
     return 0;
 }
@@ -302,7 +302,7 @@ int zmk_rgb_underglow_on() {
 
     state.on = true;
     state.animation_step = 0;
-    k_timer_start(&underglow_tick, K_NO_WAIT, K_MSEC(50));
+    k_timer_start(&underglow_tick, K_NO_WAIT, K_MSEC(25));
 
     return zmk_rgb_underglow_save_state();
 }
