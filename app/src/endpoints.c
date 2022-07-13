98fc053cba777bc0c88615b3400beeec894a17ee
Donald Gordon <donald@gordon.nz>
2022-07-13 22:39:44 +1200

    RGB underglow status support
    Adds Glove80's status indicator using RGB underglow support. Requires ZMK
    PR#999 and PR#1243.
    
    The underglow status is able to show layer state, battery levels,
    caps/num/scroll-lock, BLE and USB state. The underglow positions selected for
    each of these indicators is configured using the new devicetree node
    zmk,underglow-indicators, which takes an array of integer LED positions for each
    feature.

--- app/src/endpoints.c
+++ app/src/endpoints.c
@@ -320,6 +320,10 @@ static struct zmk_endpoint_instance get_selected_instance(void) {
     return instance;
 }
 
+bool zmk_endpoints_preferred_transport_is_active(void) {
+    return preferred_transport == get_selected_transport();
+}
+
 static int zmk_endpoints_init(void) {
 #if IS_ENABLED(CONFIG_SETTINGS)
     settings_subsys_init();
